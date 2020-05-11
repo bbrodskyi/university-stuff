add5980085992246a92101ad2ca803800a6f97f0
Bohdan Brodskyi <bohdan.brodskyi@jmmj.pl>
2020-05-11 09:36:37 +0200

    Set up project for Inner Classes task

---
+++ sem-2/gui/inner-classes-21491/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int s = 1;
+        out.println(s);
+
+    }
+}
